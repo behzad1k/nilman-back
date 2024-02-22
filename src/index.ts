3628e9644994a32f203215d2f17cc083a1dc7d7c
behzad1k <bhzd1k@gmail.com>
2024-02-22 03:59:09 +0330

    cors origin

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,9 @@ class Server {
     this.app.use(passport.initialize());
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
-    this.app.use(cors());
+    this.app.use(cors({
+      origin: ['https://app.nilman.co', 'http://app.nilman.co']
+    }));
     this.app.use(helmet());
   }
 
