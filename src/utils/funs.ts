bf81636df1641adc4469f8092abd88b6e51c7978
behzad 1k <bhzd1k@gmail.com>
2025-02-13 09:03:33 +0330

    ap

--- src/utils/funs.ts
+++ src/utils/funs.ts
@@ -147,7 +147,7 @@ export const decrypt = (
     const ivBuffer = Buffer.from(iv, 'base64');
 
     const decipher = createDecipheriv(
-      'aes-256-ccm',
+      'aes-256-ctr',
       keyBuffer,
       ivBuffer
     );
