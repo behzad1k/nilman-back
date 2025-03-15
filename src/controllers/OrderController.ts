03a7b59fce854da676f81c6ed64c9c59111c8796
behzad 1k <bhzd1k@gmail.com>
2025-03-15 11:50:33 +0330

    fixes

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -267,11 +267,12 @@ class OrderController {
     } = req.body;
     let user, serviceObj, attributeObjs: Service[] = [], addressObj, worker, discountObj: Discount;
     if ((!isUrgent ? date == moment().format('jYYYY/jMM/jDD') || (date == moment().add(1, 'd').format('jYYYY/jMM/jDD') && Number(moment().add(24, 'h').format('HH')) > time) : false) ||
+      (date == '1403/12/30') ||
       (date == moment().format('jYYYY/jMM/jDD') && Number(moment().format('HH')) > (time - 5)) ||
       (date == moment().add(1, 'd').format('jYYYY/jMM/jDD') && Number(moment().format('HH')) >= 16 && Number(moment().format('HH')) < 18 && time < 10) ||
       (date == moment().add(1, 'd').format('jYYYY/jMM/jDD') && Number(moment().format('HH')) >= 18 && time < 12)) {
       res.status(400).send({
-        code: 1012,
+        code: 1020,
         data: 'Invalid Date'
       });
       return;
