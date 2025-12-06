a3c2824fcbd3a5fd95a622dab5ba5024f1ecf1d2
behzad 1k <bhzd1k@gmail.com>
2025-12-06 05:15:29 +0330

    faster + migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,13 +2,15 @@ import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_HOST);
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST || "0.0.0.0",
     port: parseInt(process.env.DB_PORT) || 3306,
     username: process.env.DB_USERNAME || "behzad1k",
-    password: process.env.DB_PASSWORD || "behzaD!1",
+    password: process.env.DB_PASSWORD || "h/#0mpzP7Vi3mQzs",
     database: process.env.DB_NAME || "nil",
     synchronize: false,
     logging: false,
