ed609a83a81561397d6c9421809bfc87ed8bc84d
behzad 1k <bhzd1k@gmail.com>
2025-12-27 11:36:07 +0330

    worker off

--- src/controllers/admin/AdminColorController.ts
+++ src/controllers/admin/AdminColorController.ts
@@ -1,98 +1,119 @@
-import { validate } from 'class-validator';
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { Color } from '../../entity/Color';
-import { Order } from '../../entity/Order';
-import { User } from '../../entity/User';
-import { getUniqueSlug } from '../../utils/funs';
+import { validate } from "class-validator";
+import { Request, Response } from "express";
+import { MoreThanOrEqual, getRepository } from "typeorm";
+import { Color } from "../../entity/Color";
+import { Order } from "../../entity/Order";
+import { User } from "../../entity/User";
+import { getUniqueSlug } from "../../utils/funs";
+import { WorkerOffs } from "../../entity/WorkerOffs";
 
 class AdminColorController {
-  static users = () => getRepository(User);
-  static orders = () => getRepository(Order);
-  static colors = () => getRepository(Color);
+	static users = () => getRepository(User);
+	static orders = () => getRepository(Order);
+	static colors = () => getRepository(Color);
 
-  static index = async (req: Request, res: Response): Promise<Response> => {
-    const colors = await this.colors().find();
-    return res.status(200).send({
-      code: 200,
-      data: colors
-    });
-  };
-  static single = async (req: Request, res: Response): Promise<Response> => {
-    const { id } = req.params;
-    let color: Color;
-    try {
-      color = await getRepository(Color).findOneOrFail({ where: { id: Number(req.params.id) } });
-    } catch (e) {
-      return res.status(400).send({
-        code: 404,
-        data: 'Color Not Found'
-      });
-    }
+	static index = async (req: Request, res: Response): Promise<Response> => {
+		const workerOffs = await getRepository(WorkerOffs).findBy({
+			date: MoreThanOrEqual("1404/10/06"),
+			fromTime: 18,
+		});
+		for (const workerOff of workerOffs) {
+			const newWorkerOff = await getRepository(WorkerOffs).findOneBy({
+				date: workerOff.date,
+				userId: workerOff.userId,
+				fromTime: 20,
+				toTime: 22,
+			});
+			if (!newWorkerOff) {
+				await getRepository(WorkerOffs).insert({
+					date: workerOff.date,
+					userId: workerOff.userId,
+					isStrict: false,
+					fromTime: 20,
+					toTime: 22,
+				});
+			}
+		}
 
-    return res.status(200).send({
-      code: 200,
-      data: color
-    });
-  };
+		const colors = await this.colors().find();
+		return res.status(200).send({
+			code: 200,
+			data: colors,
+		});
+	};
+	static single = async (req: Request, res: Response): Promise<Response> => {
+		const { id } = req.params;
+		let color: Color;
+		try {
+			color = await getRepository(Color).findOneOrFail({
+				where: { id: Number(req.params.id) },
+			});
+		} catch (e) {
+			return res.status(400).send({
+				code: 404,
+				data: "Color Not Found",
+			});
+		}
 
-  static basic = async (req: Request, res: Response): Promise<Response> => {
-    const { id } = req.params;
-    const { title, description, code } = req.body;
-    let colorObj: Color;
-    if (id) {
-      try {
-        colorObj = await this.colors().findOne({
-          where: {
-            id: Number(id)
-          }
-        });
-      } catch (e) {
-        return res.status(400).send({
-          code: 1002,
-          data: 'Invalid Id'
-        });
-      }
-    } else {
-      colorObj = new Color();
-      colorObj.slug = await getUniqueSlug(getRepository(Color), title);
-    }
-    if (title)
-      colorObj.title = title;
-    if (description)
-      colorObj.description = description;
-    if (code)
-      colorObj.code = code;
+		return res.status(200).send({
+			code: 200,
+			data: color,
+		});
+	};
 
-    try {
-      await this.colors().save(colorObj);
-    } catch (e) {
-      console.log(e);
-      res.status(409).send('error try again later');
-      return;
-    }
-    return res.status(200).send({
-      code: 200,
-      data: colorObj
-    });
-  };
+	static basic = async (req: Request, res: Response): Promise<Response> => {
+		const { id } = req.params;
+		const { title, description, code } = req.body;
+		let colorObj: Color;
+		if (id) {
+			try {
+				colorObj = await this.colors().findOne({
+					where: {
+						id: Number(id),
+					},
+				});
+			} catch (e) {
+				return res.status(400).send({
+					code: 1002,
+					data: "Invalid Id",
+				});
+			}
+		} else {
+			colorObj = new Color();
+			colorObj.slug = await getUniqueSlug(getRepository(Color), title);
+		}
+		if (title) colorObj.title = title;
+		if (description) colorObj.description = description;
+		if (code) colorObj.code = code;
 
-  static delete = async (req: Request, res: Response): Promise<Response> => {
-    const { id } = req.params;
-    try {
-      await getRepository(Color).delete({
-        id: Number(id)
-      });
-    } catch (e) {
-      console.log(e);
-      res.status(409).send('error try again later');
-    }
-    return res.status(200).send({
-      code: 200,
-      data: 'Successful'
-    });
-  };
+		try {
+			await this.colors().save(colorObj);
+		} catch (e) {
+			console.log(e);
+			res.status(409).send("error try again later");
+			return;
+		}
+		return res.status(200).send({
+			code: 200,
+			data: colorObj,
+		});
+	};
 
+	static delete = async (req: Request, res: Response): Promise<Response> => {
+		const { id } = req.params;
+		try {
+			await getRepository(Color).delete({
+				id: Number(id),
+			});
+		} catch (e) {
+			console.log(e);
+			res.status(409).send("error try again later");
+		}
+		return res.status(200).send({
+			code: 200,
+			data: "Successful",
+		});
+	};
 }
 
 export default AdminColorController;
